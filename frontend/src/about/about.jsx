50004cc5d58da7478377fbddc0cd256d4bc51e35
unknown <magalhaeskleo@gmail.com>
2018-05-09 15:10:03 -0300

    'com07-09-05-2018'

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+
+export default props =>(
+    <div>
+        <PageHeader name='Sobre' small='Nós'></PageHeader>
+        <h2>Nossa Historia </h2>
+        <p>Lorem ipsun sit amet ...</p>
+        <p>Lorem ipsun sit amet ...</p>
+
+        <h2>Imprensa </h2>
+        <p>Loremvv ipsun sit amet ...</p>
+        <p>Lorem ipsun sit amet fdasaf sf cddsfsfsd...</p>
+    </div>    
+
+
+)
